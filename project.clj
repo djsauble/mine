5641361c313b06eda0462891821c6bdc6d80397e
Daniel Sauble <djsauble@puppetlabs.com>
2012-03-17 16:48:07 -0700

    Adds description and namespace to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject mine "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+(defproject com.github.djsauble2.mine "0.1.0-SNAPSHOT"
+            :description "Mine is a file registry, designed to provide proof about which files are yours."
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
             :main mine.server)
-
